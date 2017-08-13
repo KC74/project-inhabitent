1d98b9e86c18f3ea5c4449d6a76efa374ba00ab6
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-08-13 14:40:54 -0700

    styled entry-title for journal posts.

--- themes/inhabitent/template-parts/content.php
+++ themes/inhabitent/template-parts/content.php
@@ -12,14 +12,16 @@
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
+		<!-- <div class="entry-header__container"> -->
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+			<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_posted_by(); ?>
+			</div><!-- .entry-meta -->
+			<?php endif; ?>
+		<!-- </div> -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
