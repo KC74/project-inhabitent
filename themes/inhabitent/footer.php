8c4b3e11190b92af55aa99a5f1c1101a0996540c
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-08-13 23:01:31 -0700

    finished footer.

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -7,16 +7,47 @@
 
 ?>
 
-			</div><!-- #content -->
+            </div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+            <footer id="colophon" class="site-footer" role="contentinfo">
+                <div class="site-info">
+                    <div class="footer__contact">
+                        <div class="footer__contact-container">
+							<h3 class="footer__title-h3">Contact Info</h3>
+							<div class="footer-contact"><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></div>
+							<div class="footer-contact"><i class="fa fa-phone" aria-hidden="true"></i><a href="tel:778-456-7891">778-456-7891</a></div>
+							<div class="footer-container">
+								<div class="footer-contact"><i class="fa fa-facebook-square" aria-hidden="true"></i></div>
+								<div class="footer-contact"><i class="fa fa-twitter-square" aria-hidden="true"></i></div>
+								<div class="footer-contact"><i class="fa fa-google-plus-square" aria-hidden="true"></i></div>
+							</div>
+						</div>
+                    </div>
+                    <div class="footer__business-hours">
+					<div class="business-hours">
+						<h3 class=footer__title-h3>Business Hours</h3>
+						<p><span class="font-bold">Monday-Friday:</span> 9am to 5pm</p>
 
-		<?php wp_footer(); ?>
+						<p><span class="font-bold">Saturday:</span> 10am to 2pm</p>
 
-	</body>
+						<p><span class="font-bold">Sunday:</span> Closed</p>
+						</div>
+                    </div>
+                    <div class="footer__logo">
+						<div class="footer__logo-container">
+							<a href="<?php echo inhabitent_the_url()?>"><img src="../wp-content/themes/inhabitent/assets/images/logos/inhabitent-logo-text.svg" class="footer__logo-img"/></a>
+						</div>
+                    </div>
+                </div><!-- .site-info -->
+                <div class="footer__copyright">
+                    <div class="container">
+						<p>COPYRIGHT © 2016 INHABITENT</p>
+					</div>
+                </div>
+            </footer><!-- #colophon -->
+        </div><!-- #page -->
+
+        <?php wp_footer(); ?>
+
+    </body>
 </html>
