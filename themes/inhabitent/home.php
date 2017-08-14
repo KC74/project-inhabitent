8198845debfc9fec4a099886f51fff922ac39103
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-08-14 10:06:45 -0700

    removed some floats in the navigation.

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -7,7 +7,7 @@
 get_header(); ?>
 
     <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+        <main id="home" class="site-home" role="main">
 		
 					<?php if ( is_home() && ! is_front_page() ) : ?>
 						<header>
@@ -18,7 +18,7 @@ get_header(); ?>
 					<?php /* Start the Loop */ ?>
 					<?php while ( have_posts() ) : the_post(); ?>
 		
-						<?php get_template_part( 'template-parts/content' ); ?>
+						<?php get_template_part( 'template-parts/content', 'home' ); ?>
 		
 					<?php endwhile; ?>
 		
