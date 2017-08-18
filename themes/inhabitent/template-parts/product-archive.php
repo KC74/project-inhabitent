994985065df0e87c56752921671f4dbc68f9fa6c
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-08-18 13:17:19 -0700

    finished product archive grid laout and positions.

--- themes/inhabitent/template-parts/product-archive.php
+++ themes/inhabitent/template-parts/product-archive.php
@@ -10,9 +10,15 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
         <?php if ( has_post_thumbnail() ) : ?>
+        <div class="product-thumbnail-wrapper">
           <a href="<? echo get_post_permalink() ?>"><?php the_post_thumbnail( 'large' ); ?></a>
+        </div>
         <?php endif; ?>
         <!-- <div class="entry-header__container"> -->
-        <?php the_title(); echo CFS()->get( 'product_price' );?>
+        <div class="product-info">
+            <h2 class="product-title"><?php the_title(); ?></h2>
+            <span class="product-price"><?php echo CFS()->get( 'product_price' ); ?></span>
+            <span class="product-info-separator">...........................................................................................</span>
+        </div>
     </header><!-- .entry-header -->
 </article><!-- #post-## -->
