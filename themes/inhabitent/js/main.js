cb59620cf66b6c52b074e12b62f1040a51f61bbc
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-08-21 02:19:12 -0700

    finished styling and functionality of menu search.

--- themes/inhabitent/js/main.js
+++ themes/inhabitent/js/main.js
@@ -1,3 +1,30 @@
-function test() {
-    
-}
+/**
+ * main.js
+ *
+ * Helps with accessibility for keyboard only users.
+ *
+ * Learn more: https://github.com/Automattic/Inhabitent Starter Theme/pull/136
+ */
+// (function () {
+    var searchButton = document.getElementById('search-button');
+    var searchField = document.getElementById('search');
+
+    searchButton.onclick = function() {
+        searchField.classList.toggle('search-expanded');
+        searchField.focus();
+        searchField.setAttribute("value", "");
+    };
+
+   searchField.onblur = function() {
+        searchField.classList.toggle('search-collapse');
+        setTimeout(function(){
+            //do what you need here
+            searchField.classList.remove('search-expanded');
+            searchField.classList.toggle('search-collapse');
+            searchField.blur();
+            searchField.setAttribute("value", "");
+        }, 550);
+        
+   }
+
+// })();
