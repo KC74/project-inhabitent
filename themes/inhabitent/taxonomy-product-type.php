f6db3e1bf9e28ee619aa3a50e10206d178f4cbdb
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-08-20 19:53:28 -0700

    finished adding single taxonomy page.

---
+++ themes/inhabitent/taxonomy-product-type.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying taxonomy of product types.
+ *
+ * @package Inhabitent_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+                <h1 class="product-tax-title"><?php single_term_title(); ?></h1>
+                <div class="product-tax-description"><?php echo term_description(); ?></div>
+			</header><!-- .page-header -->
+
+            <?php /* Start the Loop */ ?>
+            
+            <div class="product-archive-grid">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <div class="product-grid-item">
+                        <?php
+                            get_template_part( 'template-parts/product', 'archive' );
+                        ?>
+                    </div>
+
+                <?php endwhile; ?>
+            </div>
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
